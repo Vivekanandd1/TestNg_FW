cb337cf8d53f073a0eb9a6b34e3ffd8def0c2fb3
vivekanandd1 <vivekanandd1@live.com>
2022-03-19 13:57:50 +0530

    commit test5th in Branch 1

---
+++ TestNg_FW/src/TestNg_Suite/_5_FifthTest.java
@@ -0,0 +1,13 @@
+package TestNg_Suite;
+
+public class _5_FifthTest {
+	
+	public void fifthcase() {
+		System.out.println("we are testing eclipse");
+	}
+	
+	public void fifthtest() {
+		System.out.println("we are mananging Branches");
+	}
+
+}
